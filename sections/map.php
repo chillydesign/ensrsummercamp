a6c8a85aebcd9eb47765c3ed2661832c85b2c31f
Charles Harvey <harvey.charles@gmail.com>
2018-07-17 11:51:34 +0200

    updated google maps api key

--- sections/map.php
+++ sections/map.php
@@ -8,7 +8,7 @@
 </div>
 
 
-<script type='text/javascript' src='//maps.google.com/maps/api/js?key=AIzaSyC-BDJZU14ltCrYRPei33a4ZSQfJqRbxNY&#038;ver=4.8.1'></script>
+<script type='text/javascript' src='//maps.google.com/maps/api/js?key=AIzaSyAxQfqRqtPLAW4BolFMCxTiv9y--R8CXdU&#038;ver=4.8.1'></script>
 <script type="text/javascript">
 var map_title = 'Champery';
 var map_location = {lat: 46.1813333, lng:6.8742121};
