f108ebdcc4923eb149bf5a33d55e7f375f006577
Charles Harvey <harvey.charles@gmail.com>
2019-04-01 12:50:43 +0200

    replace gmaps key

--- sections/map.php
+++ sections/map.php
@@ -7,8 +7,8 @@
     </div>
 </div>
 
-
-<script type='text/javascript' src='//maps.google.com/maps/api/js?key=AIzaSyAxQfqRqtPLAW4BolFMCxTiv9y--R8CXdU&#038;ver=4.8.1'></script>
+<?php $map_key = 'AIzaSyB8R30nSBXyDaB5tjb_WLLabbGkCnp_R2c'; ?>
+<script type='text/javascript' src='//maps.google.com/maps/api/js?key=<?php echo $map_key; ?>'></script>
 <script type="text/javascript">
 var map_title = 'Champery';
 var map_location = {lat: 46.1813333, lng:6.8742121};
